3dc101b27d4d4c04571f1f1cdc58ecb73da45810
ShmarinaV <59163840+ShmarinaV@users.noreply.github.com>
2021-01-02 12:32:47 +0300

    Add files via upload

---
+++ src/components/Pages/Main/Main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+import Autorization from '../Authorization/Autorization';
+
+
+class MainPage extends React.Component{  
+
+  render(){
+    return(
+      <div>
+        
+      
+        <Autorization/>
+      </div>
+    )
+  }
+}
+
+export default MainPage;
